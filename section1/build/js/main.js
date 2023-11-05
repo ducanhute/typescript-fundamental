1c9033eb96d6f1a02f4a5ec4553a9528a23d32e8
ducanhute <anhhdtech@gmail.com>
2023-11-06 04:59:07 +0700

    first commit

---
+++ section1/build/js/main.js
@@ -0,0 +1,5 @@
+"use strict";
+let userName = "Ha duc Anh 1";
+console.log(userName);
+let a = 3;
+let b = '2';
---
+++ section1/src/main.ts
@@ -0,0 +1,4 @@
+let userName = "Ha duc Anh 1"
+console.log(userName)
+let a = 3
+let b = '2'
